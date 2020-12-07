9001ff3e4951fe91d8c48e3ea80e87808a67f656
manwithhonor <sharapov.roman@gmail.com>
2020-12-07 16:58:27 +0300

    Revert "refactor of arduino libraries"
    This reverts commit 1885b00997da56b320f458425ba25199facd5109.

--- platform/sam3x/platform.cpp
+++ platform/sam3x/platform.cpp
@@ -1,26 +1,28 @@
+#include "DueFlashStorage.h"
+#include "rtc_clock.h"
+#include "./../bitcoin/Bitcoin.h"
 #include "./../../src/platform/platform.h"
 
-#include <DueFlashStorage.h>
-#include <RTCDue.h>
-
-RTCDue rtc(XTAL);
+RTC_clock rtc_clock(RC); // Select the Slowclock source
 DueFlashStorage dueFlashStorage;
 
 namespace platform {
     namespace clocks {
         void init() {
-            rtc.begin();
+            rtc_clock.init();               //init real time clock to write proper time while recording events
+            rtc_clock.set_time(__TIME__);   //__TIME__ and __DATA__ are taken from timestamp when this code was compiled
+            rtc_clock.set_date(__DATE__);   //if __DATA__ is 1.1.2007 then this is a problem of compiler  
         }
 
         platform::clocks::Time get() {
             platform::clocks::Time time;
     
-            time.seconds = rtc.getSeconds();
-            time.minutes = rtc.getMinutes();
-            time.hours   = rtc.getHours();
-            time.day     = rtc.getDay();
-            time.month   = rtc.getMonth();
-            time.year    = rtc.getYear();
+            time.seconds = rtc_clock.get_seconds();
+            time.minutes = rtc_clock.get_minutes();
+            time.hours   = rtc_clock.get_hours();
+            time.day     = rtc_clock.get_days();
+            time.month   = rtc_clock.get_months();
+            time.year    = rtc_clock.get_years();
             
             return time;
         }
