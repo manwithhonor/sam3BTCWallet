2c565431bb4cc2549ab873bb259ad76d7555d091
manwithhonor <sharapov.roman@gmail.com>
2020-12-14 18:52:51 +0300

    fix port recognition

--- tests/connection_test.py
+++ tests/connection_test.py
@@ -1,10 +1,10 @@
 import serial
 import time
 
-
-
-# serialcomm = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
-serialcomm = serial.Serial('COM3', baudrate=115200, timeout=1)
+try:
+    serialcomm = serial.Serial('/dev/ttyACM1', baudrate=115200, timeout=1)
+except:
+    serialcomm = serial.Serial('COM3', baudrate=115200, timeout=1)
 time.sleep(2)
 
 text = "0_0"
