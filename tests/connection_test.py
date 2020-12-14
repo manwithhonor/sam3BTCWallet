ecb21ca3f93ddcf0fd3dc865ed5a1742b5918517
manwithhonor <sharapov.roman@gmail.com>
2020-12-14 18:44:17 +0300

    add connection test

---
+++ tests/connection_test.py
@@ -0,0 +1,36 @@
+import serial
+import time
+
+
+
+# serialcomm = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
+serialcomm = serial.Serial('COM3', baudrate=115200, timeout=1)
+time.sleep(2)
+
+text = "0_0"
+serialcomm.write(text.encode())
+time.sleep(2)
+print("Response is:", serialcomm.read_all().decode('ascii'))
+
+text = "1_m/0/1"
+serialcomm.write(text.encode())
+time.sleep(2)
+print("Response is:", serialcomm.read_all().decode('ascii'))    
+
+text = "1_m/0/5"
+serialcomm.write(text.encode())
+time.sleep(2)
+print("Response is:", serialcomm.read_all().decode('ascii'))  
+
+text = "2_m/0/1"
+serialcomm.write(text.encode())
+time.sleep(2)
+print("Response is:", serialcomm.read_all().decode('ascii'))
+
+text = "3_0"
+serialcomm.write(text.encode())
+time.sleep(2)
+print("Response is:", serialcomm.read_all().decode('ascii'))
+
+serialcomm.close()
+print("END OF TEST")
